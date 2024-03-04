151d3c37efa335d05df97d377ae892e82a63472c
Feryal Doubali <feryalhalima@gmail.com>
2024-03-04 18:13:28 +0100

    done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    let currentIndex = this.questions.length - 1;
+    while (currentIndex >= 0) {
+      let randomIndex = Math.floor(Math.random() * this.currentQuestionIndex);
+      let currentCell = this.questions[this.currentQuestionIndex];
+      let randomCell = this.questions[randomIndex];
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+      let copyTemp = currentCell;
+      this.questions[currentIndex] = randomCell;
+      this.questions[randomIndex] = copyTemp;
+      currentIndex--;
+    }
+  }
+  checkAnswer(answer) {
+    this.correctAnswers++;
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
