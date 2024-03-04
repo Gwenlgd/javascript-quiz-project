faf01cc07d6dfc0f1e9f008e79646d29d5caa72f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-03-04 17:00:41 +0100

    Finished questions file

--- src/question.js
+++ src/question.js
@@ -1,7 +1,29 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
 
-    // 2. shuffleChoices()
+  }
+  // 2. shuffleChoices()
+
+  shuffleChoices() {
+    let currentIndex = this.choices.length - 1;
+
+    while (currentIndex >= 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      let currentCell = this.choices[currentIndex];
+      let randomCell = this.choices[randomIndex];
+
+      let copyTemp = currentCell;
+      this.choices[currentIndex] = randomCell;
+      this.choices[randomIndex] = copyTemp;
+
+      currentIndex--;
+    }
+  }
 }
